22f69063496851bdd4791e4ef0a23659c6ef25d5
Eric Furze <efurze@yahoo-inc.com>
2017-01-23 18:41:27 -0800

    More svg stuff

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -11,7 +11,9 @@ $(function() {
 
 		render: function() {
 			var draw = SVG('svg').size(300,300);
-			var rect = draw.rect(100,100).attr({fill: 'blue'});
+			var rect = draw.rect(100,100).attr({fill: 'yellow'});
+			var text = draw.text("lorem ipsum");
+			text.transform({scaleX: 0.5, scaleY: 0.5});
 		}
 
 	};
